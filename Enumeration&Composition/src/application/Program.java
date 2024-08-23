3a4132a0bcd55420f10d8c7229b41def7326ebfd
Victor Barboza <victor.barboza052008@gmail.com>
2024-08-23 15:33:03 -0300

    updated program

--- Enumeration&Composition/src/application/Program.java
+++ Enumeration&Composition/src/application/Program.java
@@ -54,7 +54,6 @@ public class Program {
 		System.out.println("Department: " + worker.getDepartment().getName());
 		System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
 		
-		System.out.println();
 		
 		sc.close();
 		
